77a1bc6ba76998eccb31b35600c0c5bbf404c910
John Binning <john.binning17@gmail.com>
2017-09-28 13:02:31 -0600

    add score functionality

--- scripts/rps.js
+++ scripts/rps.js
@@ -1,10 +1,19 @@
-let streak;
+const score = {
+  streak: 0,
+  computer: 0,
+  user: 0
+}
 
-const streakCalc = (result) => {
+const scoreCalc = (result) => {
+  console.log(result.includes('win'))
   if (result.includes('win')) {
-    streak += 1;
+    score.streak += 1;
+    score.user += 1;
+  } else if (result.includes('Tie')) {
+    score.streak = 0;
   } else {
-    streak = 0;
+    score.streak = 0;
+    score.computer += 1;
   }
 }
 
@@ -31,29 +40,61 @@ const rockPaperScissors = (rps) => {
   if (rps === computer) {
     outcome = `Tie!  You both picked ${rps}`
   } 
-  streakCalc(outcome);
+  scoreCalc(outcome);
   return outcome;
 }
 
-const updateStreak = () => {
-  $('#streak-val').html(streak);
+const updateScore = () => {
+  $('#streak-val').html(score.streak);
+  $('#user-score-val').html(score.user);
+  $('#computer-score-val').html(score.computer);
+  
+}
+
+const setBg = () => {
+  const bgNum = Math.floor(Math.random() * (7 - 1)) + 1;
+  switch(bgNum) {
+    case 1:
+      document.body.style.backgroundColor = 'chartreuse';
+      break;
+    case 2:
+      document.body.style.backgroundColor = 'aqua';
+      break;
+    case 3: 
+      document.body.style.backgroundColor = 'red';
+      break;
+    case 4: 
+      document.body.style.backgroundColor = 'grey';
+      break;
+    case 5: 
+      document.body.style.backgroundColor = 'orange';
+      break;
+    case 6: 
+      document.body.style.backgroundColor = 'violet';
+      break;
+    default:
+      document.body.style.backgroundColor = 'grey';
+  }
 }
 
 $('#rock').on('click', () => {
   $('#outcome').html('');
   $('#outcome').prepend(`<h2>${rockPaperScissors('rock')}`);
-  updateStreak();
+  updateScore();
+  // setBg();
 })
 
 $('#paper').on('click', () => {
   $('#outcome').html('');
   $('#outcome').prepend(`<h2>${rockPaperScissors('paper')}`);
-  updateStreak();
+  updateScore();
+  // setBg();
 })
 
 $('#scissors').on('click', () => {
   $('#outcome').html('');
   $('#outcome').prepend(`<h2>${rockPaperScissors('scissors')}`);
-  updateStreak();
+  updateScore();
+  // setBg();
 })
 
